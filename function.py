16430b3b53fdb09f1e0cf316501128469ef486a8
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-06-16 11:08:09 -0400

    Basic modules and flow control guess game

---
+++ function.py
@@ -0,0 +1,10 @@
+def hello(name):
+    print("Hello " + name)
+
+hello('Alice')
+hello('Bob')
+
+def plusOne(num):
+    return num + 1
+
+print(plusOne(5))
---
+++ module.py
@@ -0,0 +1,8 @@
+import random, sys, os, math
+# from random import *
+
+print(random.randint(1,10))
+# print(randint(1,10))
+
+print('Hello')
+# sys.exit()
