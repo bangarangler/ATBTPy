cd5a601fcdf87da758474f9547ce838e6ee451e1
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-06-19 11:54:20 -0400

    File w

---
+++ files.py
@@ -0,0 +1,8 @@
+# baconFile = open("bacon.txt", "a")
+# baconFile.write("\n\nBacon is delicious.")
+# baconFile.close()
+
+# import shelve
+# shelFile = shelve.open("mydata")
+# shelFile["cats"] = ["Zophie", "Pooka", "Simon", "Fat-tail", "Cleo"]
+# shelFile.close()
